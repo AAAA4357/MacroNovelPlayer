4c5c382dfbce26ba7f57a56429311f52d61c5279
AAAA4357 <1113064715@qq.com>
2025-11-08 17:10:18 +0800

    Missed pp options

--- Assets/Scripts/Core/DOTS/Systems/PostProcessingSystem.cs
+++ Assets/Scripts/Core/DOTS/Systems/PostProcessingSystem.cs
@@ -15,6 +15,16 @@ namespace MNP.Core.DOTS.Systems
             {
                 managedProperty1DComponent.RefValue.Value = property1DComponent.Value;
             }).WithoutBurst().Run();
+            Entities.WithAll<InitializedPropertyComponent>().ForEach((ManagedAnimationProperty2DComponent managedProperty2DComponent,
+                                                                      in Property2DComponent property2DComponent) =>
+            {
+                managedProperty2DComponent.RefValue.Value = property2DComponent.Value;
+            }).WithoutBurst().Run();
+            Entities.WithAll<InitializedPropertyComponent>().ForEach((ManagedAnimationProperty3DComponent managedProperty3DComponent,
+                                                                      in Property3DComponent property3DComponent) =>
+            {
+                managedProperty3DComponent.RefValue.Value = property3DComponent.Value;
+            }).WithoutBurst().Run();
         }
     }
 }
