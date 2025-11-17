fc31fa8343e3dcd263ba78ee6eb5c6cbcdfe829d
AAAA4357 <1113064715@qq.com>
2025-11-17 15:01:29 +0800

    Fix small bug

--- Assets/Scripts/Core/DOTS/Systems/PreprocessingSystem.cs
+++ Assets/Scripts/Core/DOTS/Systems/PreprocessingSystem.cs
@@ -23,26 +23,31 @@ namespace MNP.Core.DOTS.Systems
         public void OnUpdate(ref SystemState state)
         {
             EntityCommandBuffer ecb = new(Allocator.TempJob);
+            EntityCommandBuffer ecb2D = new(Allocator.TempJob);
+            EntityCommandBuffer ecb3D = new(Allocator.TempJob);
             PreprocessTransform2DJob transform2DJob = new()
             {
-                ecbWriter = ecb.AsParallelWriter()
+                ecbWriter = ecb2D.AsParallelWriter()
             };
-            JobHandle Job2DHandle = transform2DJob.ScheduleParallel(state.Dependency);
+            JobHandle job2DHandle = transform2DJob.ScheduleParallel(state.Dependency);
             PreprocessTransform3DJob transform3DJob = new()
             {
-                ecbWriter = ecb.AsParallelWriter()
+                ecbWriter = ecb3D.AsParallelWriter()
             };
-            JobHandle Job3DHandle = transform3DJob.ScheduleParallel(state.Dependency);
-            state.Dependency = JobHandle.CombineDependencies(Job2DHandle, Job3DHandle);
-            state.CompleteDependency();
+            JobHandle job3DHandle = transform3DJob.ScheduleParallel(state.Dependency);
             PreprocessJob job = new()
             {
                 ecbWriter = ecb.AsParallelWriter()
             };
-            state.Dependency = job.ScheduleParallel(state.Dependency);
+            JobHandle jobHandle = job.ScheduleParallel(state.Dependency);
+            state.Dependency = JobHandle.CombineDependencies(jobHandle, job2DHandle, job3DHandle);
             state.CompleteDependency();
             ecb.Playback(state.EntityManager);
+            ecb2D.Playback(state.EntityManager);
+            ecb3D.Playback(state.EntityManager);
             ecb.Dispose();
+            ecb2D.Dispose();
+            ecb3D.Dispose();
         }
 
         [BurstCompile]
